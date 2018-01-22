f43f91444e5ac6457c5bf707c011ffd76b173186
REDE\everton.pessoa <everton.pessoa@ativo.com>
2018-01-22 18:11:47 -0200

    atualização rotas

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,19 @@ $app->group(['prefix' => 'br'], function () use ($app) {
 
     // kits 
     $app->group(['prefix' => 'kits'], function () use ($app) {
-              $app->get('valores/{id_evento:[0-9]+}', 'KitController@valores');
+        $app->get('valores/{id_evento:[0-9]+}', 'KitController@valores');
+    });
+
+    
+    // eventos 
+    $app->group(['prefix' => 'eventos'], function () use ($app) {
+        $app->get('getById/{id_evento:[0-9]+}', 'EventoController@getById');
+    });
+
+
+    // mobile
+    $app->group(['prefix' => 'mobile'], function () use ($app) {
+        $app->get('eventos/', 'MobileController@eventos');
     });
 });
 
