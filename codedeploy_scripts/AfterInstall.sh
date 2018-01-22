1f12338b76d5379ed55df604d7586512a8d508c1
Rodolfo Silva <rodolfo.silva@ativo.com>
2018-01-22 16:55:38 -0200

    Correção de AfterInstall

--- codedeploy_scripts/AfterInstall.sh
+++ codedeploy_scripts/AfterInstall.sh
@@ -7,6 +7,6 @@ rm -f /var/www/ativos/webservices.ativo.com_temp/appspec.yml
 [ -d /var/www/ativos/webservices.ativo.com ] && mv /var/www/ativos/webservices.ativo.com /var/www/ativos/webservices.ativo.com_old || echo "Diretorio nao existe"
 mv /var/www/ativos/webservices.ativo.com_temp /var/www/ativos/webservices.ativo.com
 # Copia o arquivo de configuração do banco de dados
-[ -f /var/www/ativos/webservices.ativo.com_old/wp-config.php ] && cp -pf /var/www/ativos/webservices.ativo.com_old/wp-config.php /var/www/ativos/webservices.ativo.com/ || echo "Arquivo nao existe"
+[ -f /var/www/ativos/webservices.ativo.com_old/.env ] && cp -pf /var/www/ativos/webservices.ativo.com_old/.env /var/www/ativos/webservices.ativo.com/ || echo "Arquivo nao existe"
 # Copia o arquivo .htaccess
 [ -f /var/www/ativos/webservices.ativo.com_old/.htaccess ] && cp -pf /var/www/ativos/webservices.ativo.com_old/.htaccess /var/www/ativos/webservices.ativo.com/ || echo "Arquivo nao existe"
