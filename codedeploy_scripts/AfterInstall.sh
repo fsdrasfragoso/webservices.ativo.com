b4c10c598d803ed8554ed18383f851d102d40c95
Rodolfo Silva <rodolfo.silva@ativo.com>
2018-05-16 14:17:48 -0300

    AfterInstall corrigido - Final

--- codedeploy_scripts/AfterInstall.sh
+++ codedeploy_scripts/AfterInstall.sh
@@ -7,7 +7,7 @@ rm -f /var/www/ativos/webservices.ativo.com_temp/appspec.yml
 [ -d /var/www/ativos/webservices.ativo.com ] && mv /var/www/ativos/webservices.ativo.com /var/www/ativos/webservices.ativo.com_old || echo "Diretorio nao existe"
 mv /var/www/ativos/webservices.ativo.com_temp /var/www/ativos/webservices.ativo.com
 # Copia o arquivo de configuração do banco de dados
-[ -d /var/www/ativos/webservices.ativo.com_old/config/snappy.php ] && cp -prf /var/www/ativos/webservices.ativo.com_old/config/snappy.php /var/www/ativos/webservices.ativo.com/config/ || echo "Diretorio nao existe"
-[ -d /var/www/ativos/webservices.ativo.com_old/.env] && cp -prf /var/www/ativos/webservices.ativo.com_old/.env /var/www/ativos/webservices.ativo.com/ || echo "Arquivo não existe"
+[ -f /var/www/ativos/webservices.ativo.com_old/config/snappy.php ] && cp -prf /var/www/ativos/webservices.ativo.com_old/config/snappy.php /var/www/ativos/webservices.ativo.com/config/ || echo "Diretorio nao existe"
+[ -f /var/www/ativos/webservices.ativo.com_old/.env ] && cp -prf /var/www/ativos/webservices.ativo.com_old/.env /var/www/ativos/webservices.ativo.com/ || echo "Arquivo não existe"
 # Copia o arquivo .htaccess
 [ -f /var/www/ativos/webservices.ativo.com_old/public/.htaccess ] && cp -pf /var/www/ativos/webservices.ativo.com_old/public/.htaccess /var/www/ativos/webservices.ativo.com/public/ || echo "Arquivo nao existe"
