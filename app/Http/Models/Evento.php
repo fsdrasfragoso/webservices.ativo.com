19494ce3bddf69a380a2796bb42918a23578822a
fsdrasfragoso <esdrasfragoso@gmail.com>
2020-07-02 22:28:18 -0300

    Vou utilizar o ADM para essa parte do projeto

--- app/Http/Models/Evento.php
+++ app/Http/Models/Evento.php
@@ -416,64 +416,7 @@ class Evento {
             return $dadosCliente;
     }
 
-    static function freedomImagem(){
-        $curl = curl_init();
-        
-        $obj = array(
-            "email" => "paulo@paulo.com",
-            "senha" => "123456"
-        );
-
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://admin.99run.com/api.json",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_POSTFIELDS => json_encode($obj),
-          CURLOPT_HTTPHEADER => array(            
-            'Content-Type:application/json'
-          ),
-        ));
-      
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-      
-        curl_close($curl);
-        if(empty($err)){
-            $ch = curl_init("http://adm.ativo.com/cron/imagemfreedom");
-            $nr_peito = app('request')->input('nr_peito'); 
-            $cfile = new CURLFile(app('request')->file('arquivo'), app('request')->file('arquivo')->getClientMimeType(), 'arquivo');
-            $params = array(
-                'arquivo' => $cfile,
-                'nr_peito' => $nr_peito               
-            );
-            
-            curl_setopt_array($ch, array(
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_POST => true,
-                CURLOPT_INFILESIZE => $_FILES['arquive']['size'],
-                CURLOPT_POSTFIELDS => $params,
-                CURLOPT_HTTPHEADER => array(                    
-                    "Content-Type: multipart/form-data;"
-                ),
-            ));
-        
-            $content = curl_exec($ch);
-            $err = curl_error($ch);
-      
-        curl_close($ch);
-            if(empty($err)){
-                return $content;
-            }
-
-            return  $err;
-
-        }
-        return json_decode($err);
-     }
+    
 
 
     static function run99($infoIdEvento){
